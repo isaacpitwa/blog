30cae25dc6daf16e4969cb6f14acbab3070d660d
Isaac Pitwa <isaacpitwa256@gmail.com>
2024-09-04 15:19:49 +0300

    Chore: Add custom font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,38 +1,54 @@
 import React, { ReactNode } from "react";
 import Link from "next/link";
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: "Page Title",
+  description: "Page Description",
+};
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <header className="py-4 flex space-x-4 items-center fixed z-50 bg-gray-300 w-screen px-44">
-        <h1 className="text-3xl font-bold mb-0 ">BlogApp</h1>
-        <nav className="flex-1">
-          <ul className="flex space-x-4 items-center justify-center">
-            <li>
-              <Link href="/" className="text-blue-500 hover:text-blue-700">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/posts" className="text-blue-500 hover:text-blue-700">
-                Posts
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/posts/new"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                Add Post
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <div className={`${poppins.variable}`}>
+        <header className="py-4 flex space-x-4 items-center fixed z-50 bg-gray-300  w-screen px-80">
+          <h1 className="text-3xl font-bold mb-0 ">BlogApp</h1>
+          <nav className="flex-1">
+            <ul className="flex space-x-4 items-center justify-end">
+              <li>
+                <Link href="/" className="text-blue-500 hover:text-blue-700">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/posts"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Posts
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/posts/new"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Add Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
       <main className="pt-24 max-w-5xl mx-auto">{children}</main>
     </div>
   );
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -29,9 +29,9 @@ const PostDetails: React.FC = () => {
     return <div className="text-center mt-8">Post not found</div>;
 
   return (
-    <div className="max-w-2xl mx-auto mt-4">
+    <div className="max-w-4xl mx-auto mt-4">
       <div className="flex space-x-4 mb-12 justify-between">
-        <Link href="/" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 flex items-center space-x-2">
+        <Link href="/" className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 flex items-center space-x-2 hover:cursor-pointer ">
         <MdOutlineKeyboardBackspace />
           <label >Back to List</label>
         </Link>
